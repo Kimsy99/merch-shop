554e57de15e7e34d2e6ad84fc5c486638ff4e8c0
kimsy <kimshengyong@gmail.com>
2020-10-17 13:45:49 +0800

    Added routing function to menu-item

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
 import React from "react";
 import "./App.css";
-import Homepage from "./pages/homepage/homepage.component";
+import { Route } from "react-router-dom";
+import HomePage from "./pages/homepage/homepage.component";
 
+const HatsPage = () => {
+  return (
+    <div>
+      <hi>Hats Page hereeeee</hi>
+    </div>
+  );
+};
 function App() {
   return (
     <div className="">
-      <Homepage />
+      <Route exact path="/" component={HomePage} />
+      <Route exact path="/hats" component={HatsPage} />
     </div>
   );
 }
--- src/components/menu-item/menu-item.component.jsx
+++ src/components/menu-item/menu-item.component.jsx
@@ -1,7 +1,9 @@
 import React from 'react';
+import {withRouter} from 'react-router-dom'; // it is a higher order component => a func that takes component as arg and return a modifier component
+
 import "./menu-item.styles.scss"
-const MenuItem = ({title, imageUrl, size}) => ( //directly destructor the title from props
-  <div className={`${size} menu-item`}>
+const MenuItem = ({title, imageUrl, size, linkUrl,history, match}) => ( //directly destructor the title from props
+  <div className={`${size} menu-item`} onClick={() => history.push(`${match.url}${linkUrl}`)}>
     <div className='background-image' style={{backgroundImage: `url(${imageUrl})`}} />
     <div className='content'>
       <h1 className='title'>{title.toUpperCase()}</h1>
@@ -9,4 +11,4 @@ const MenuItem = ({title, imageUrl, size}) => ( //directly destructor the title 
     </div>
   </div>
 )
-export default MenuItem;
+export default withRouter(MenuItem); // it will return a superpowered menuitem component, then we now has access to history
