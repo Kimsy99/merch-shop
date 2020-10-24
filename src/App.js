a32294ce46f0e9fbf5cf4bd667c75e2f735d2b9c
Kim Sheng Yong <kimshengyong@gmail.com>
2020-10-24 11:45:01 +0800

    Adding header component to all poges

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
 import React from "react";
 import "./App.css";
-import { Route } from "react-router-dom";
+import { Switch, Route } from "react-router-dom";
 import HomePage from "./pages/homepage/homepage.component";
 import ShopPage from "./pages/shop/shop.component";
+import Header from "./components/header/header.components.jsx";
 
 const HatsPage = () => {
   return (
@@ -14,9 +15,12 @@ const HatsPage = () => {
 function App() {
   return (
     <div className="">
-      <Route exact path="/" component={HomePage} />
-      <Route exact path="/hats" component={HatsPage} />
-      <Route exact path="/shop" component={ShopPage} />
+      <Header />
+      <Switch>
+        <Route exact path="/" component={HomePage} />
+        <Route exact path="/hats" component={HatsPage} />
+        <Route exact path="/shop" component={ShopPage} />
+      </Switch>
     </div>
   );
 }
