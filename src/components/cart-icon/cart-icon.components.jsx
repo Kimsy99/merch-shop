c81cf1d4144fb2bc4fbc148d368a00e6ea229103
Kim Sheng Yong <kimshengyong@gmail.com>
2020-10-31 12:41:58 +0800

    implement cart item counting, using .reduce (which will rerender everytime even no changes)

--- src/components/cart-icon/cart-icon.components.jsx
+++ src/components/cart-icon/cart-icon.components.jsx
@@ -5,13 +5,19 @@ import "./cart-icon.styles.scss";
 import { connect } from "react-redux";
 import { toggleCartHidden } from "../../redux/cart/cart.actions";
 
-const CartIcon = ({ toggleCartHidden }) => (
+const CartIcon = ({ toggleCartHidden, itemCount }) => (
   <div className="cart-icon" onClick={toggleCartHidden}>
     <ShoppingIcon className="shopping-icon" />
-    <span className="item-count">0</span>
+    <span className="item-count">{itemCount}</span>
   </div>
 );
 const mapDispatchToProps = (dispatch) => ({
   toggleCartHidden: () => dispatch(toggleCartHidden()),
 });
-export default connect(null, mapDispatchToProps)(CartIcon);
+const mapStateToProps = ({ cart: { cartItems } }) => ({
+  itemCount: cartItems.reduce(
+    (count, cartItem) => count + cartItem.quantity,
+    0
+  ),
+});
+export default connect(mapStateToProps, mapDispatchToProps)(CartIcon);
