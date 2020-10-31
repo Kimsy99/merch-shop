0a7d9e5f4ddd8574f2e74a3d529588fc43e1012c
Kim Sheng Yong <kimshengyong@gmail.com>
2020-10-31 21:34:42 +0800

    add add minus quantity of product in cart

--- src/redux/cart/cart.selectors.js
+++ src/redux/cart/cart.selectors.js
@@ -24,7 +24,7 @@ export const selectCartItemsCount = createSelector(
 );
 export const selectCartTotal = createSelector([selectCartItems], (cartItems) =>
   cartItems.reduce(
-    (count, cartItem) => (count + cartItem.quantity) * cartItem.price,
+    (count, cartItem) => count + cartItem.quantity * cartItem.price,
     0
   )
 );
--- src/redux/cart/cart.util.js
+++ src/redux/cart/cart.util.js
@@ -12,3 +12,21 @@ export const addItemToCart = (cartItem, cartItemToAdd) => {
   }
   return [...cartItem, { ...cartItemToAdd, quantity: 1 }];
 };
+//if not last item them -1 only, else if last item then remove the whole thing
+export const removeItemFromCart = (cartItems, cartItemToRemove) => {
+  const existingCartItem = cartItems.find(
+    (cartItem) => cartItem.id === cartItemToRemove.id
+  );
+  //if last then remove it
+  if (existingCartItem.quantity === 1) {
+    return cartItems.filter(
+      (item) => item.id !== cartItemToRemove.id //remain those that are not we wanna remove
+    );
+  }
+  //if not last then -1 from quantity
+  return cartItems.map((cartItem) =>
+    cartItem.id === cartItemToRemove.id
+      ? { ...cartItem, quantity: cartItem.quantity - 1 }
+      : cartItem
+  );
+};
