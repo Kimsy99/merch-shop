4e2ba1d7d8003d50a8bff4ec767cad4abdcf3962
Kim Sheng Yong <kimshengyong@gmail.com>
2020-11-01 13:08:54 +0800

    Add session storage persistence usign redux-persist

--- src/redux/root-reducer.js
+++ src/redux/root-reducer.js
@@ -1,9 +1,15 @@
 import { combineReducers } from "redux";
-
+import { persistReducer } from "redux-persist";
+import storage from "redux-persist/lib/storage"; //import local storage object from window browser
 import userReducer from "./user/user.reducer";
 import cartReducer from "./cart/cart.reducer";
-
-export default combineReducers({
+const persistConfig = {
+  key: "root",
+  storage,
+  whitelist: ["cart"], //the things we want persist is cart as user is handled by firebase
+};
+const rootReducer = combineReducers({
   user: userReducer,
   cart: cartReducer,
 });
+export default persistReducer(persistConfig, rootReducer); //modified version of our root reducer with persistence capabilities
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,11 +1,13 @@
 // To create the store object
 import { createStore, applyMiddleware } from "redux"; //middleware is a function that recerive action in and do something then ouput
+import { persistStore } from "redux-persist";
 import logger from "redux-logger";
 
 import rootReducer from "./root-reducer";
 
 const middleware = [logger]; //all methods we wanna put in middleware are stored as array here
 
-const store = createStore(rootReducer, applyMiddleware(...middleware));
+export const store = createStore(rootReducer, applyMiddleware(...middleware));
+export const persistor = persistStore(store); //a persisted version of our store
 
-export default store;
+export default { store, persistor };
