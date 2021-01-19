86bb52ba86f6f4e47d2dcd8ffaa025e6ab088a71
Kim Sheng Yong <kimshengyong@gmail.com>
2021-01-19 20:12:36 +0800

    add container pattern to collection and collectionPreview

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -2,18 +2,12 @@ import React from 'react';
 import { Route } from 'react-router';
 import { connect } from 'react-redux';
 import { createStructuredSelector } from 'reselect';
-import CollectionOverview from '../../components/collection-overview/collection-overview.components';
+import CollectionsOverviewContainer from '../../components/collection-overview/collection-overview.container';
 import WithSpinner from '../../components/with-spinner/with-spinner.components';
-import CollectionPage from '../collection/collection.components';
+import CollectionPageContainer from '../collection/collection.container';
 import { fetchCollectionsStartAsync } from '../../redux/shop/shop.actions';
-import {
-  selectIsCollectionFetching,
-  selectIsCollectionsLoaded,
-} from '../../redux/shop/shop.selector';
+import { selectIsCollectionsLoaded } from '../../redux/shop/shop.selector';
 
-// get the component for both overview and page pages.
-const CollectionOverviewWithSpinner = WithSpinner(CollectionOverview);
-const CollectionPageWithSpinner = WithSpinner(CollectionPage);
 class ShopPage extends React.Component {
   // obtain collection data from firestore as here shoppage ahve children -> overview , page that require that data
   componentDidMount() {
@@ -21,39 +15,37 @@ class ShopPage extends React.Component {
     fetchCollectionsStartAsync();
   }
   render() {
-    const { match, isCollectionFetching, isCollectionLoaded } = this.props;
+    const { match, isCollectionLoaded } = this.props;
     return (
       <div className='shop-page'>
         <Route
           exact
           path={`${match.path}`}
-          render={(props) => (
-            <CollectionOverviewWithSpinner
-              isLoading={isCollectionFetching}
-              {...props}
-            />
-          )}
+          // render={(props) => (
+          //   <CollectionOverviewWithSpinner
+          //     isLoading={isCollectionFetching}
+          //     {...props}
+          //   />
+          // )}
+          component={CollectionsOverviewContainer}
         />
         <Route
           path={`${match.path}/:collectionId`}
-          render={(props) => (
-            <CollectionPageWithSpinner
-              isLoading={!isCollectionLoaded} //if loaded then it is not loading
-              {...props}
-            />
-          )}
+          // render={(props) => (
+          //   <CollectionPageWithSpinner
+          //     isLoading={!isCollectionLoaded} //if loaded then it is not loading
+          //     {...props}
+          //   />
+          // )}
+          component={CollectionPageContainer}
         />
       </div>
     );
   }
 }
-const mapStateToProps = createStructuredSelector({
-  isCollectionFetching: selectIsCollectionFetching,
-  isCollectionLoaded: selectIsCollectionsLoaded,
-});
 const mapDispatchToProps = (dispatch) => ({
   fetchCollectionsStartAsync: () => dispatch(fetchCollectionsStartAsync()),
 });
-export default connect(mapStateToProps, mapDispatchToProps)(ShopPage);
+export default connect(null, mapDispatchToProps)(ShopPage);
 
 //pass category id as parameter then we we put /hats -> categoryId = hats => then we can know which page it is going and publish which one
